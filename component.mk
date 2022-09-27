a4b183b3f6867e4d2733a37d563283b8be389cfc
kuba <qb4-dev@gmail.com>
2022-09-27 22:20:16 +0200

    initial commit

---
+++ component.mk
@@ -0,0 +1,26 @@
+#
+# Component Makefile
+#
+
+COMPONENT_ADD_INCLUDEDIRS += libsupla/src
+COMPONENT_ADD_INCLUDEDIRS += libsupla/include
+
+COMPONENT_SRCDIRS += libsupla/src/supla-common
+COMPONENT_OBJS += libsupla/src/supla-common/lck.o
+COMPONENT_OBJS += libsupla/src/supla-common/log.o
+COMPONENT_OBJS += libsupla/src/supla-common/proto.o
+COMPONENT_OBJS += libsupla/src/supla-common/srpc.o
+
+COMPONENT_SRCDIRS += libsupla/src/port
+COMPONENT_OBJS += libsupla/src/port/util.o
+COMPONENT_OBJS += libsupla/src/port/net.o
+
+COMPONENT_SRCDIRS += libsupla/src
+COMPONENT_OBJS += libsupla/src/device.o
+COMPONENT_OBJS += libsupla/src/channel.o
+COMPONENT_OBJS += libsupla/src/supla-value.o
+COMPONENT_OBJS += libsupla/src/supla-extvalue.o
+COMPONENT_OBJS += libsupla/src/supla-action-trigger.o
+
+CFLAGS += -DSUPLA_DEVICE
+
