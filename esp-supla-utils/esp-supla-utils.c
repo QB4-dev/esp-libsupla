ae3ec49ff453b0262a78c2772266e1ca86086182
kuba <qb4-dev@gmail.com>
2022-10-06 22:53:24 +0200

    mdns added

--- esp-supla-utils/esp-supla-utils.c
+++ esp-supla-utils/esp-supla-utils.c
@@ -13,11 +13,14 @@
 #include <esp_log.h>
 #include <esp_wifi.h>
 #include <nvs_flash.h>
+#include <esp_netif.h>
+#include <tcpip_adapter.h>
+#include <mdns.h>
 
 static const char *TAG = "SUPLA-ESP";
 static const char *NVS_STORAGE = "supla_nvs";
 
-int supla_esp_nvs_config_init(struct supla_config *supla_conf)
+esp_err_t supla_esp_nvs_config_init(struct supla_config *supla_conf)
 {
 	size_t required_size;
 	nvs_handle nvs;
@@ -70,22 +73,66 @@ int supla_esp_nvs_config_init(struct supla_config *supla_conf)
 			ESP_LOGE(TAG, "nvs open error %s",esp_err_to_name(rc));
 		}
 	}
-	return 0;
+	return ESP_OK;
 }
 
-int supla_esp_nvs_config_erase(void)
+esp_err_t supla_esp_nvs_config_erase(void)
 {
 	nvs_handle nvs;
 	esp_err_t rc;
 	rc = nvs_open(NVS_STORAGE,NVS_READWRITE,&nvs);
 	if(rc == ESP_OK){
-		nvs_erase_all(&nvs);
-		ESP_LOGE(TAG, "nvs config erased");
+		nvs_erase_all(nvs);
+		ESP_LOGI(TAG, "nvs config erased");
 	}else{
 		ESP_LOGE(TAG, "nvs open error %s",esp_err_to_name(rc));
 	}
 	return rc;
 }
+
+esp_err_t supla_esp_generate_hostname(supla_dev_t *dev, char* buf, size_t len)
+{
+	uint8_t mac[6];
+	const char *dev_name = supla_dev_get_name(dev);
+
+	ESP_ERROR_CHECK(esp_efuse_mac_get_default(mac));
+	if(strlen(dev_name) + 18 > len)
+		return ESP_ERR_INVALID_SIZE;
+
+	snprintf(buf,len,"%s-%02X%02X",dev_name,mac[4],mac[5]);
+	return ESP_OK;
+}
+
+//esp_err_t supla_esp_set_device_hostname(supla_dev_t *dev, tcpip_adapter_if_t tcpip_if)
+//{
+//	char hostname[TCPIP_HOSTNAME_MAX_SIZE];
+//	ESP_ERROR_CHECK(supla_esp_generate_hostname(dev,hostname,sizeof(hostname)));
+//	ESP_ERROR_CHECK(tcpip_adapter_set_hostname(tcpip_if,hostname));
+//	ESP_LOGI(TAG, "device hostname: %s",hostname);
+//	return ESP_OK;
+//}
+
+
+esp_err_t supla_esp_init_mdns(supla_dev_t *dev)
+{
+#if ENABLE_MDNS == y
+	char mdns_name[SUPLA_DEVICE_NAME_MAXSIZE+16];
+
+	if(!dev)
+		return ESP_ERR_INVALID_ARG;
+
+	ESP_ERROR_CHECK(mdns_init());
+	ESP_ERROR_CHECK(supla_esp_generate_hostname(dev,mdns_name,sizeof(mdns_name)));
+	ESP_ERROR_CHECK(mdns_hostname_set(mdns_name));
+	ESP_LOGI(TAG, "mdns hostname: %s",mdns_name);
+	return ESP_OK;
+#else
+	#warning "mDNS is disabled - .local acess will not work"
+	return ESP_ERR_NOT_SUPPORTED;
+#endif
+}
+
+
 
 int supla_esp_get_wifi_state(TDSC_ChannelState *state)
 {
@@ -133,3 +180,5 @@ int supla_esp_server_time_sync(supla_dev_t *dev, TSDC_UserLocalTimeResult *lt)
 
 	return settimeofday(&timeval,NULL);
 }
+
+
--- include/esp-supla-utils.h
+++ include/esp-supla-utils.h
@@ -8,12 +8,34 @@
 #define ESP_SUPLA_UTILS_H_
 
 #include <libsupla/device.h>
+#include <esp_err.h>
 
-int supla_esp_nvs_config_init(struct supla_config *supla_conf);
-int supla_esp_nvs_config_erase(void);
 
+//TODO documentation
+esp_err_t supla_esp_nvs_config_init(struct supla_config *supla_conf);
+
+//TODO documentation
+esp_err_t supla_esp_nvs_config_erase(void);
+
+//TODO documentation
+esp_err_t supla_esp_generate_hostname(supla_dev_t *dev, char* buf, size_t len);
+
+//TODO documentation
+esp_err_t supla_esp_init_mdns(supla_dev_t *dev);
+
+
+//TODO config handlers GET/POST
+
+//TODO documentation
 int supla_esp_get_wifi_state(TDSC_ChannelState *state);
 
+//TODO documentation
 int supla_esp_server_time_sync(supla_dev_t *dev, TSDC_UserLocalTimeResult *lt);
+
+
+
+
+//TODO set device name as hostname
+//TODO start wifi_AP with devce name+mac
 
 #endif /* ESP_SUPLA_UTILS_H_ */
